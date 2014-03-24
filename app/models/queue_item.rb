9125d7216e11e8e4001fac61ff9c703d25685a73
Zac Clay <zacclay@gmail.com>
2014-03-24 16:29:52 -0400

    Added queue reordering

--- app/models/queue_item.rb
+++ app/models/queue_item.rb
@@ -5,7 +5,7 @@ class QueueItem < ActiveRecord::Base
   delegate :title, to: :video, prefix: :video
 
   validates_uniqueness_of :user_id, scope: [:video_id]
-
+  validates_numericality_of :position, {only_integer: true}
   def rating
     review = Review.where(user_id: user_id, video_id: video.id).first
     review.rating if review
