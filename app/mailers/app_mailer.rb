5e6ea8ac8040bbf00ad9a53ffd6728e1a95f9eb7
Zac Clay <zacclay@gmail.com>
2014-04-09 11:49:13 -0400

    Email update

--- app/mailers/app_mailer.rb
+++ app/mailers/app_mailer.rb
@@ -1,12 +1,12 @@
 class AppMailer < ActionMailer::Base
   def send_welcome_email(user)
     @user = user
-    mail to: user.email, from: "me@test.com", subject: "Welcome to MyFlix yo!"
+    mail to: user.email, from: "info@zacflix.herokuapp.com", subject: "Welcome to MyFlix yo!"
   end
 
   def send_forgot_password(user)
     @user = user
-    mail to: user.email, from: "me@test.com", subject: "Password Reset Request For Your Account"
+    mail to: user.email, from: "me@zacflix@herokuapp.com", subject: "Password Reset Request For Your Account"
   end
 
   def send_invitation_email(invitation)
